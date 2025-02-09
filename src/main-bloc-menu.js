5ddf35c7714392b758246939b0ff9aa11f6e412c
LostGlass <ilya.shyrsin.ykr.net@gmail.com>
2025-02-10 00:24:30 +0200

    10.02.2025

--- src/main-bloc-menu.js
+++ src/main-bloc-menu.js
@@ -3,7 +3,7 @@ import "./style/css/main-bloc-menu.css";
 function MainBlocMenuRender() {
   return (
     <div className="wrapp_main_bloc_menu">
-      <div className=" main_menu_logo"></div>
+      <div className="main_menu_logo"></div>
     </div>
   );
 }
